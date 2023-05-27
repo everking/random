7a0ea52b8673829649b1a7e7ecdb72f37b8d27fc
Eric de Guzman <eric_deguzman@bmc.com>
2023-05-26 19:26:08 -0700

    Persist data locally

--- kubernetes/local-rancher/create-container.sh
+++ kubernetes/local-rancher/create-container.sh
@@ -2,6 +2,7 @@ docker run -d \
   --name local-rancher \
   --restart=unless-stopped \
   -p 80:80 -p 443:443 \
+  -v /opt/rancher:/var/lib/rancher \
   -v ${PWD}/rancher-pub.pem:/etc/rancher/ssl/cert.pem \
   -v ${PWD}/rancher-key.pem:/etc/rancher/ssl/key.pem \
   -v ${PWD}/rancher-pub.pem:/etc/rancher/ssl/cacerts.pem \
