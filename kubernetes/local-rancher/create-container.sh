e9ac75ff698e35a8497dd2734ea2d4f76640b67e
Eric de Guzman <eric_deguzman@bmc.com>
2023-05-27 17:46:05 -0700

    Change rancher data folder

--- kubernetes/local-rancher/create-container.sh
+++ kubernetes/local-rancher/create-container.sh
@@ -2,7 +2,7 @@ docker run -d \
   --name local-rancher \
   --restart=unless-stopped \
   -p 8080:80 -p 8443:443 \
-  -v /opt/rancher:/var/lib/rancher \
+  -v ${HOME}/dev/rancher:/var/lib/rancher \
   -v ${HOME}/dev/rancher/rancher-pub.pem:/etc/rancher/ssl/cert.pem \
   -v ${HOME}/dev/rancher/rancher-key.pem:/etc/rancher/ssl/key.pem \
   -v ${HOME}/dev/rancher/rancher-pub.pem:/etc/rancher/ssl/cacerts.pem \
