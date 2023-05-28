a0b60f9ba60ef2f5f9aecc0d91973ee86e0cf48e
Eric de Guzman <eric_deguzman@bmc.com>
2023-05-27 19:08:22 -0700

    Create data folder

--- kubernetes/local-rancher/create-container.sh
+++ kubernetes/local-rancher/create-container.sh
@@ -1,8 +1,22 @@
+
+export DATA_FOLDER="${HOME}/rancher-data"
+
+[ ! -d ${DATA_FOLDER} ]
+then
+  mkdir -p ${DATA_FOLDER}
+  sudo chown root:root ${DATA_FOLDER}
+  touch ${DATA_FOLDER}/sample-data.txt
+  echo "Created folder ${DATA_FOLDER}."
+  ls -ltrh ${DATA_FOLDER}
+else
+  echo "${DATA_FOLDER} already exists."
+fi
+
 docker run -d \
   --name local-rancher \
   --restart=unless-stopped \
   -p 8080:80 -p 8443:443 \
-  -v ${HOME}/dev/rancher:/var/lib/rancher \
+  -v ${DATA_FOLDER}:/var/lib/rancher \
   -v ${HOME}/dev/rancher/rancher-pub.pem:/etc/rancher/ssl/cert.pem \
   -v ${HOME}/dev/rancher/rancher-key.pem:/etc/rancher/ssl/key.pem \
   -v ${HOME}/dev/rancher/rancher-pub.pem:/etc/rancher/ssl/cacerts.pem \
