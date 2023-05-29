b4798b5cec252ab622ee3e56d98ffeb6b2b45e75
Eric de Guzman <eric_deguzman@bmc.com>
2023-05-29 06:46:57 -0700

    Save to letsencrypt

--- home/certbot-dry-run.sh
+++ home/certbot-dry-run.sh
@@ -1,1 +1,2 @@
-docker compose run --rm  certbot certonly --webroot --webroot-path /var/www/certbot/ --dry-run -d m634.dev
+docker compose run --rm  certbot certonly \
+  --webroot --webroot-path /var/www/certbot/ --dry-run -d m634.dev
