6ffada31ff52279192f7dd897600e906205ca3e9
everking <eric.deguzman@outlook.com>
2023-06-02 01:00:37 -0700

    ingress changes

--- kubernetes/local-rancher/create-container.sh
+++ kubernetes/local-rancher/create-container.sh
@@ -15,11 +15,13 @@ fi
 docker run -d \
   --name local-rancher \
   --restart=unless-stopped \
-  -p 9090:80 -p 9443:443 \
+  -p 80:30080 -p 443:30443 \
+  -p 9090:9090 -p 9443:9443 \
   -p 30000-32767:30000-32767 \
   -v ${DATA_FOLDER}:/var/lib/rancher \
   -v ${HOME}/dev/random/home/certbot/conf/archive/m634.dev/fullchain3.pem:/etc/rancher/ssl/cert.pem \
   -v ${HOME}/dev/random/home/certbot/conf/archive/m634.dev/privkey3.pem:/etc/rancher/ssl/key.pem \
   -v ${HOME}/dev/random/home/certbot/conf/archive/m634.dev/fullchain3.pem:/etc/rancher/ssl/cacerts.pem \
+  -v ${PWD}/entrypoint.sh:/usr/bin/entrypoint.sh \
   --privileged \
   rancher/rancher:latest
